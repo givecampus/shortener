2f30e4e56e24fdf4824adec345eb79ac007bc05c
Nicholas Mullen <nicholasmullen@Nicholass-MBP.fios-router.home>
2021-07-19 09:40:03 -0400

    Removes debugging print statement

--- app/models/shortener/shortened_url.rb
+++ app/models/shortener/shortened_url.rb
@@ -62,7 +62,6 @@ class Shortener::ShortenedUrl < ActiveRecord::Base
   # we'll rely on the DB to make sure the unique key is really unique.
   # if it isn't unique, the unique index will catch this and raise an error
   define_method CREATE_METHOD_NAME do
-    puts "Hitting the create method"
     count = 0
     begin
       self.unique_key = generate_unique_key
